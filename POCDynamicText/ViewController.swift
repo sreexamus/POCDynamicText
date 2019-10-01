0c2cd6725201a69f3f78cf7ea5faac71508faf3a
Sreekanth Reddy <sreekanthreddy.iragamreddy@united.com>
2019-10-01 10:55:00 -0500

    Initial Commit

---
+++ POCDynamicText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POCDynamicText
+//
+//  Created by sreekanth reddy iragam reddy on 10/1/19.
+//  Copyright © 2019 United Airlines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
